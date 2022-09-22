33bb06d839bbc6875f1555a7a54978e929975a48
JJmartinc <jorge.martin.carrion@gmail.com>
2022-09-22 13:55:14 +0200

    pal lobby

---
+++ prueba.js
@@ -0,0 +1,50 @@
+// Solicita al usuario que elija entre la opción 1 o 2:
+// Se le debe pedir dos números al usuario e indicar si son pares y múltiplos de 25 ambos, uno de ellos o ninguno.
+// Se le debe pedir una palabra y comprobar si empieza o termina por a
+
+
+
+//? Primero tenemos que crear dos botones y dentro de cada boton crear cada una de las funciones
+//? Para el primer ejercicio lo hay que ver si  los numeros introducidos son pares y multiplos de 25
+
+function numeros()
+
+var n1 = prompt("Inserta tu primer numero: ");
+var n2 = prompt("Inserta un seguno numero: ");
+
+if((n1%2)==0)
+console.log("Es un numero par")
+{
+    else {
+        console.log("Es un numero impar")
+    }
+}
+if((n2%2)==0)
+console.log("Es un numero par"){
+    else console.log("Es un numero impar")
+}
+
+
+
+
+
+
+
+
+
+
+
+function numeros()
+
+let palabra = prompt("Introduce una palabra");
+
+
+if(palabra[0]=='a'){
+    .alert("Empieza por a"){
+    } else if (palabra[-1]=='a'){
+        console.log("termina por a"){
+        } else {
+            console.log("No termina ni empieza por a")
+        }
+        }
+    }
